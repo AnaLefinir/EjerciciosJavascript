97f7312a81e9aafa0d6fd36069a69bedddc1f400
Ana Lefinir <analefinir@gmail.com>
2016-12-04 23:50:13 -0300

    Terminado ejercicio noveno de Javascript: obtener cuando dias faltan para navidad desde el dia de hoy

--- Basic/src/main.js
+++ Basic/src/main.js
@@ -12,7 +12,7 @@ function sayPepe() {
 function currentDayandTime() {
     var date = new Date();
     var day = date.getDay();
-    var dayList = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+    var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
@@ -62,10 +62,9 @@ function currentDayandTime() {
 }
 
 
-
 /*2. Write a JavaScript program to print the contents of the current window */
 
-function printCurrentPage(){
+function printCurrentPage() {
     window.print();
 }
 
@@ -73,12 +72,12 @@ function printCurrentPage(){
 /*3. Write a JavaScript program to get the current date.
  Expected Output :
  mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyyu
-*/
+ */
 
-function currentDate(){
+function currentDate() {
     var date = new Date();
-    var day = date.getDate() <= 9 ? '0'+date.getDate() : date.getDate();
-    var month = (date.getMonth()+1) <= 9 ? '0'+(date.getMonth()+1) : (date.getMonth() + 1);
+    var day = date.getDate() <= 9 ? '0' + date.getDate() : date.getDate();
+    var month = (date.getMonth() + 1) <= 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
     var year = date.getFullYear();
     var strOne = day + '/' + month + '/' + year;
     var strTwo = day + '-' + month + '-' + year;
@@ -92,9 +91,9 @@ function currentDate(){
 
 /* 4. Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7. */
 
-function areaTriangle(a, b, c){
-    var s = (a+b+c)/2;
-    var innerCount = s*(s-a)*(s-b)*(s-c);
+function areaTriangle(a, b, c) {
+    var s = (a + b + c) / 2;
+    var innerCount = s * (s - a) * (s - b) * (s - c);
     var area = Math.sqrt(innerCount);
 
     console.log(area);
@@ -107,36 +106,35 @@ function areaTriangle(a, b, c){
  from the end of the string and attaching it to the front */
 
 
-function rotateString(string){
+function rotateString(string) {
     var stringToRotate = string;
     var stringLength = stringToRotate.length;
 
-    for(var i = 0; i< stringLength; i++){
+    for (var i = 0; i < stringLength; i++) {
         var lastCharacter = stringToRotate.slice(-1);
-        var strWithoutLastCharacter =stringToRotate.slice(0, -1);
+        var strWithoutLastCharacter = stringToRotate.slice(0, -1);
 
-        stringToRotate= lastCharacter + strWithoutLastCharacter;
+        stringToRotate = lastCharacter + strWithoutLastCharacter;
         console.log(stringToRotate);
     }
 
-    console.log("the result is: "+ stringToRotate);
+    console.log("the result is: " + stringToRotate);
     return stringToRotate;
 }
 
 
-
 /* 6.Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar */
 
-function isLeapYear(number){
+function isLeapYear(number) {
     var year = number;
-    var response= "";
-    var yearIsInteger = Number.isInteger(year/4);
+    var response = "";
+    var yearIsInteger = Number.isInteger(year / 4);
     console.log(yearIsInteger);
 
-    if(yearIsInteger){
+    if (yearIsInteger) {
         response = "Yes, is a leap year";
-    }else{
-        response ="No, is not a leap year";
+    } else {
+        response = "No, is not a leap year";
     }
 
     console.log(response);
@@ -148,14 +146,14 @@ function isLeapYear(number){
  *7. Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050
  */
 
-function  findAllFistJanuaryMonday(){
+function findAllFistJanuaryMonday() {
     var start = new Date("01/01/2014");
-    var end = new Date ("01/01/2050");
+    var end = new Date("01/01/2050");
     var allJanuaryFistMonday = [];
 
-    for(start; start <= end ; start.setFullYear(start.getFullYear()+1)){
+    for (start; start <= end; start.setFullYear(start.getFullYear() + 1)) {
 
-        if(start.getDay() === 0){
+        if (start.getDay() === 0) {
             allJanuaryFistMonday.push(start.toString());
         }
     }
@@ -173,25 +171,44 @@ function  findAllFistJanuaryMonday(){
  */
 
 
-function isMatches (){
+function isMatches() {
     var userNumber = prompt("Please enter a number between 1 to 10", "1");
     var randomNumber = Math.floor(Math.random() * 10 + 1);
     var result;
 
     userNumber = parseInt(userNumber);
 
-    if (userNumber === randomNumber){
+    if (userNumber === randomNumber) {
         result = "Good Work";
-    }else{
+    } else {
         result = "Not matched";
     }
 
     return result;
 }
 
+/**
+ * 9. Write a JavaScript program to calculate days left until next Christmas.
+ *
+ */
+
+function dayLeftToChristmas() {
+    var today = new Date();
+    var christmasDay = newDate("12/25/2016");
+    var oneDay = 86400000;
+
+    today = today.getTime();
+    christmasDay = christmasDay2.getTime();
 
 
+    var differenceInMs = christmasDay  - today;
 
+    var daysLeftToChristmas = Math.round(differenceInMs / oneDay);
+
+    console.log(daysLeftToChristmas);
+    //return daysLeftToChristmas;
+
+}
 
 
 module.exports = {
