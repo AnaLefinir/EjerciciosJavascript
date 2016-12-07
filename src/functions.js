cf3f13bc53033b033ff31930585a559af0d1dc1e
Ana Lefinir <analefinir@gmail.com>
2016-12-07 06:04:31 -0300

    Iniciando ejercicios de Funciones con sus respectivos test

---
+++ src/functions.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Anita on 07/12/2016.
+ */
---
+++ test/testFunctions.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Anita on 07/12/2016.
+ */
+var assert = require("assert"),
+    functionsJavascript = require("../src/functions.js"),
+    sinon = require('sinon');
+
+describe('functions', function () {
+    it('should do something', sinon.test(function () {}));
+});
