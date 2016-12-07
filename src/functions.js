78865388171335bdab76275034633b9fbe08bb9c
Ana Lefinir <analefinir@gmail.com>
2016-12-07 07:07:08 -0300

    Primeras dos funciones con sus respectivos test de Functions Javascript

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,56 @@
 /**
  * Created by Anita on 07/12/2016.
  */
+/**
+ * 1. Write a JavaScript function that reverse a number.
+ */
+
+function reverseANumber(number){
+    var result;
+    var number = number;
+
+    if(number.length === 1){
+        result = number;
+    }else{
+        var strNumber = number.toString();
+        var toReverse = strNumber.split("").reverse().join("");
+        var reverseNumber = Number(toReverse);
+
+        result = reverseNumber;
+    }
+
+    return result;
+}
+
+
+/**
+ *Write a JavaScript function that checks whether a passed string is palindrome or not
+ */
+
+function isPalindrome(string){
+    var stringToAnalize = string.replace(" ", "");
+    var result;
+
+    var analize = stringToAnalize.split("").reverse().join("");
+
+    if(stringToAnalize === analize){
+        result = true;
+    }else{
+        result = false;
+    }
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
+module.exports = {
+    reverseANumber: reverseANumber,
+    isPalindrome: isPalindrome
+}
--- test/testFunctions.js
+++ test/testFunctions.js
@@ -5,6 +5,26 @@ var assert = require("assert"),
     functionsJavascript = require("../src/functions.js"),
     sinon = require('sinon');
 
-describe('functions', function () {
-    it('should do something', sinon.test(function () {}));
+describe('reverseANumber', function () {
+    it('should return "321" from "123"', function () {
+        assert.deepStrictEqual(321, functionsJavascript.reverseANumber(123));
+    });
+
+    it('should return "1" from "1"', function () {
+        assert.deepStrictEqual(1, functionsJavascript.reverseANumber(1));
+    });
+});
+
+describe('isPalindrome', function () {
+    it('should return a true from "ana"', function () {
+        assert.deepStrictEqual(true, functionsJavascript.isPalindrome("ana"));
+    });
+
+    it('should return a false from "dos"', function () {
+        assert.deepStrictEqual(false, functionsJavascript.isPalindrome("dos"));
+    });
+
+    it('should return a false from "nurses run"', function () {
+        assert.deepStrictEqual(true, functionsJavascript.isPalindrome("nurses run"));
+    });
 });
