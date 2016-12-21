627f25e84ccfdc188eb988e3cc6a131de6c2e631
AnaLefinir <analefinir@gmail.com>
2016-12-21 07:07:28 -0300

    Ejercicios 3 de recursion NO tiene test

--- src/recursion.js
+++ src/recursion.js
@@ -61,7 +61,7 @@ function integerBetween (a, b){
 
 /**
  *
- * 
+ *
  */
 
 
--- test/testRecursion.js
+++ test/testRecursion.js
@@ -5,44 +5,43 @@ var assert = require("assert"),
     recursionJavascript = require("../src/recursion.js"),
     sinon = require('sinon');
 
-describe('factorial', function(){
-    it('should return 6, from input 3', function(){
+describe('factorial', function () {
+    it('should return 6, from input 3', function () {
         var resultExpected = 6;
         assert.deepStrictEqual(resultExpected, recursionJavascript.factorial(3));
     });
 
-    it('should return 1, from input 1', function(){
+    it('should return 1, from input 1', function () {
         var resultExpected = 1;
         assert.deepStrictEqual(resultExpected, recursionJavascript.factorial(1));
     });
 });
 
-describe('gcd', function(){
-    it('should return 91 from calculate gcd(2366, 273)', function(){
+describe('gcd', function () {
+    it('should return 91 from calculate gcd(2366, 273)', function () {
         var resultExpected = 91;
         assert.deepStrictEqual(resultExpected, recursionJavascript.gcd(2366, 273));
 
     });
 });
 
-describe('integerBetween', function(){
-    it('should return [2,3,4]', function(){
-        var resultExpected = [2,3,4];
-         assert.deepStrictEqual(resultExpected, recursionJavascript.integerBetween(1,5));
+describe('integerBetween', function () {
+    /*it('should return [2,3,4]', function () {
+        var range = [];
+        recursionJavascript.integerBetween(1, 5);
+        var resultExpected = [2, 3, 4];
+        assert.deepStrictEqual(resultExpected, range);
+    });*/
+});
+
+describe('', function () {
+    it('', function () {
 
     });
 });
 
-describe('', function(){
-    it('', function(){
-        /*var resultExpected;
-         assert.deepStrictEqual(resultExpected, functionsJavascript);
-         */
-    });
-});
-
-describe('', function(){
-    it('', function(){
+describe('', function () {
+    it('', function () {
         /*var resultExpected;
          assert.deepStrictEqual(resultExpected, functionsJavascript);
          */
