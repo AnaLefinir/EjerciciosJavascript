4da781de1135a31c2cc151d5c66a28c43537437f
AnaLefinir <analefinir@gmail.com>
2017-01-03 15:18:38 -0300

    Inicio Ejercicio Loops and Statements

---
+++ src/statements and loops.js
@@ -0,0 +1,20 @@
+/**
+ * Created by anita on 3/1/2017.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+
+};
---
+++ test/testStatementsAndLoops.js
@@ -0,0 +1,24 @@
+/**
+ * Created by anita on 3/1/2017.
+ */
+var assert = require("assert"),
+    loopsJavascript = require("../src/statements and loops.js"),
+    sinon = require('sinon');
+
+describe('...', function () {
+    it('',function () {
+        var resultExpect;
+        assert.deepStrictEqual(resultExpect, loopsJavascript);
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
